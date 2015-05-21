17b94dca81914e31b6e47ac9496dd86140b5f525
Øyvind Nordbø <oyvinor@gmail.com>
2015-05-21 12:04:04 +0200

    Convert buttons to anchor tags

--- js/index.js
+++ js/index.js
@@ -1,24 +1,17 @@
 //Animate viewport on click
 $('#discover').click(function() {
 	$('html, body').animate({
-		scrollTop: $('#spotify').offset().top //Set destination target
-	}, 700); //Set the animation duration
+		scrollTop: $('#spotify').offset().top // Set destination target
+	}, 600); // Set the animation duration
 });
 
-//Fade out
-$('button, #about-link').click(function() {
-	$('.wrap').fadeOut(250); // Set the animation duration
-});
-
-
-
-//TODO: Deal with button shit
-$('container > a').click(function (e) {
+//Fade out links
+$('a').not('#discover').click(function (e) {
 	e.preventDefault();						// Prevent default anchor behavior
 	var goTo = this.getAttribute("href");	// Store anchor href
 		$('.wrap').fadeOut(250);			// Animate while timer ticks
 
 	setTimeout(function(){
 		window.location = goTo;	// Fetch previously stored href
-	}, 300);	// Set anchor timeout
+	}, 300); // Set anchor timeout
 });
