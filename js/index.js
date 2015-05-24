e72ced15b4f1c00cbe6dd81abc2eab37aaf2a61d
Øyvind Nordbø <oyvinor@gmail.com>
2015-05-24 18:32:34 +0200

    Add webfont and wowJS

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-//Animate viewport on click
-$('#discover').click(function() {
-	$('html, body').animate({
-		scrollTop: $('#spotify').offset().top // Set destination target
-	}, 600); // Set the animation duration
-});
+$(document).ready(function() {
+	//Animate viewport on click
+	$('#discover').click(function() {
+		$('html, body').animate({
+			scrollTop: $('#spotify').offset().top // Set destination target
+		}, 600); // Set the animation duration
+	});
 
-//Fade out links
-$('a').not('#discover').click(function (e) {
-	e.preventDefault();						// Prevent default anchor behavior
-	var goTo = this.getAttribute("href");	// Store anchor href
-		$('.wrap').fadeOut(250);			// Animate while timer ticks
+	//Fade out links
+	$('a').not('#discover').click(function (e) {
+		e.preventDefault();						// Prevent default anchor behavior
+		var goTo = this.getAttribute("href");	// Store anchor href
+			$('.wrap').fadeOut(250);			// Animate while timer ticks
 
-	setTimeout(function(){
-		window.location = goTo;	// Fetch previously stored href
-	}, 300); // Set anchor timeout
+		setTimeout(function(){
+			window.location = goTo;	// Fetch previously stored href
+		}, 300); // Set anchor timeout
+	});
 });
