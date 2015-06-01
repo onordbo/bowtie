80a601611ef210a136da9bfe8aa90b07dd37b84a
Øyvind Nordbø <oyvinor@gmail.com>
2015-06-02 00:18:30 +0200

    Fix font rendering (finally), Add hamburger-nav

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	});
 
 	//Fade out links
-	$('a').not('#discover').click(function (e) {
+	$('a').not('#discover').not('#trigger-overlay').click(function (e) {
 		e.preventDefault();						// Prevent default anchor behavior
 		var goTo = this.getAttribute("href");	// Store anchor href
 			$('body').fadeOut(250);			// Animate while timer ticks
@@ -17,3 +17,45 @@ $(document).ready(function() {
 		}, 300); // Set anchor timeout
 	});
 });
+
+//Nav
+(function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
+
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
+	}
+
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
