03a4f76bc36d5f479503251008466352d9cf3bdd
Øyvind Nordbø <oyvinor@gmail.com>
2016-01-29 12:08:35 +0100

    add stuff

--- js/index.js
+++ js/index.js
@@ -2,18 +2,9 @@ $(document).ready(function() {
 	//Animate viewport on click
 	$('#discover').click(function() {
 		$('html, body').animate({
-			scrollTop: $('#spotify').offset().top // Set destination target
-		}, 600); // Set the animation duration
-	});
-
-	//Fade out links
-	$('a').not('#discover').not('#trigger-overlay').not('#mail').click(function (e) {
-		e.preventDefault();		// Prevent default anchor behavior
-		var goTo = this.getAttribute("href");	// Store anchor href
-			$('body').fadeOut(250); 	// Animate while timer ticks
-
-		setTimeout(function(){
-			window.location = goTo;	// Fetch previously stored href
-		}, 300); // Set anchor timeout
+			scrollTop: $('#footer').offset().top // Set destination target
+		}, 1200); // Set the animation duration
 	});
 });
+
+console.log('%cHey there! I can see you found my code. If you like designers who code, we should talk! Email me on hello@oyvind.co or check out more of my code on GitHub – http://github.com/onordbo ', 'color: blue; font-size: 22px; font-family: sans-serif;');
